817c8d9bda6925453b48181648188a5e52587050
Siddhartha Biswas <siddhartha100@gmail.com>
2021-08-15 15:00:31 -0500

    configuring router for menu items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from '../src/pages/homepages/homepage.component';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+
+
+const HatsPage = () => {
+  return (<div><h1>Hat's Page</h1></div>)
+};
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
